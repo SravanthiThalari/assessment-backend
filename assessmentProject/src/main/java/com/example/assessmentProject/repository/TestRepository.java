51da968504718cd60ef4ee819645c2bf0c009fe3
SravanthiThalari <23eg104c32@anurag.edu.in>
2026-03-25 23:10:26 +0530

    Initial clean backend

--- assessmentProject/src/main/java/com/example/assessmentProject/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.assessmentProject.repository;
-
-import com.example.assessmentProject.model.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test,Long> {
-}
